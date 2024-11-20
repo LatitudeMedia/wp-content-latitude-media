f7b9364b293c0202cf2b3884122bc28a706ed0d0
vlad <vlad.kostetskiy@grandiz.com>
2024-11-20 15:17:22 +0200

    podcasts

--- wp-content/themes/latitudemedia/inc/media.php
+++ wp-content/themes/latitudemedia/inc/media.php
@@ -3,5 +3,5 @@
 add_image_size( 'home-top', 1300, 1424, true );
 //Single resource page
 add_image_size( 'single-resource-featured', 311, 402, true );
-add_image_size( 'resource-sponsor-logo', 159, 32, true );
+add_image_size( 'resource-sponsor-logo', 159, 32, false );
 add_image_size( 'podcast-landing-overview', 413, 310, true );
