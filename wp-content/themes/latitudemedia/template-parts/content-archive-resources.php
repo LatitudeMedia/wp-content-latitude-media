9733423f80d07e53b6c835c0b1f341d1d77cfcaf
vlad <vlad.kostetskiy@grandiz.com>
2024-12-03 16:53:53 +0200

    feedback/fixes

--- wp-content/themes/latitudemedia/template-parts/content-archive-resources.php
+++ wp-content/themes/latitudemedia/template-parts/content-archive-resources.php
@@ -22,44 +22,6 @@ $resourceForm = get_field('subscribe_form', 'options');
     <div class="container">
         <div class="resources-list-wrapper">
             <ul>
-            <?php
-            while(have_posts() && $i < 3) {
-                the_post();
-
-                get_template_part(
-                    'template-parts/components/post',
-                    'item',
-                    array(
-                        'post_id'  => get_the_ID(),
-                        'settings' => array(
-                            'thumb'   => array(
-                                'size'       => 'article-related',
-                                'link'       => true,
-                                'alt_image'  => false,
-                            ),
-                        ),
-                        'rows'     => $postItemTemplate['rows'],
-                        'wrap'     => $postItemTemplate['wrap'],
-                    )
-                );
-                wp_reset_postdata();
-                $i++;
-            }
-            ?>
-            </ul>
-        </div>
-    </div>
-</div>
-
-<?php
-    get_template_part('template-parts/components/form/type3', '', $resourceForm);
-?>
-
-<div class="resources-list pink">
-    <div class="container">
-        <?php if($i > 0) : ?>
-        <div class="resources-list-wrapper">
-            <ul>
                 <?php
                 while(have_posts()) {
                     the_post();
@@ -84,7 +46,6 @@ $resourceForm = get_field('subscribe_form', 'options');
                 ?>
             </ul>
         </div>
-        <?php endif; ?>
         <?php
             do_action('paginator', null, true);
         ?>
