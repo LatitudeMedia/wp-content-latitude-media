843ce13c6d14d7cfbb482d10fe51603b55a0e142
vlad <vlad.kostetskiy@grandiz.com>
2025-02-05 16:49:29 +0200

    - preload images

--- wp-content/themes/latitudemedia/inc/media.php
+++ wp-content/themes/latitudemedia/inc/media.php
@@ -24,33 +24,3 @@ add_image_size( 'large-event', 1270, 715, false );
 add_image_size( 'article-related-news', 329, 256, true );
 add_image_size( 'author-archive-hero', 427, 427, true );
 add_image_size( 'event-agenda', 552);
-
-if( !function_exists('add_preload_images_tag') ) {
-    add_action('wp_head', 'add_preload_images_tag');
-    function add_preload_images_tag() {
-        if( is_home() || is_front_page() ) {
-            printf('<link rel="preload" as="image" href="%1$s">', 'https://i0.wp.com/latitudemedia.mystagingwebsite.com/wp-content/uploads/2024/12/674e1a0ed3b8fb4fa0cf3daa_Latitude-1920px-32-Image-2024-12-02T153519.762.jpg?resize=338%2C270&ssl=1');
-        }
-
-//        if( is_singular('product') ) {
-//            global $product;
-//            $attachment_ids = $product->get_gallery_image_ids();
-//
-//            if(!$attachment_ids) {
-//                return;
-//            }
-//
-//            printf('<link rel="preload" as="image" href="%1$s">', wp_get_attachment_image_url( $attachment_ids[0], 'product-gallery-thumb' ));
-//        }
-//
-//        if( is_singular('post') && has_post_thumbnail() ) {
-//            if (class_exists('\Automattic\Jetpack\Image_CDN\Image_CDN_Core')) {
-//                $featuredUrl = \Automattic\Jetpack\Image_CDN\Image_CDN_Core::cdn_url(get_the_post_thumbnail_url(), array( 'fit' => "1024,551"));
-//                printf('<link rel="preload" as="image" href="%1$s">', $featuredUrl);
-//            }
-//        }
-//
-//https://i0.wp.com/latitudemedia.mystagingwebsite.com/wp-content/uploads/2024/12/674e1a0ed3b8fb4fa0cf3daa_Latitude-1920px-32-Image-2024-12-02T153519.762.jpg?resize=338%2C270&ssl=1
-//<link rel="preload" href="https://example.com/image.webp" as="image" imagesrcset="https://example.com/image.webp" imagesizes="(max-width: 338px) 100vw, 338px">
-    }
-}
