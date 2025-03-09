1215985b97daced40f6f4c54e8fdc9a31b698495
vlad <vlad.kostetskiy@grandiz.com>
2025-03-09 23:12:32 +0200

    - fix relevanssi

--- wp-content/themes/latitudemedia/inc/override.php
+++ wp-content/themes/latitudemedia/inc/override.php
@@ -10,17 +10,17 @@ function set_pagination_base () {
 add_action( 'init', 'set_pagination_base' );
 
 // Modify pagination base to use a GET parameter
-//function custom_pagination_base( $query ) {
-//    if ( !is_admin() && ($query->is_main_query() || is_tax('section')) ) {
-//        // Check if the `page` GET parameter is set
-//        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-//        if ( isset( $_GET['page'] ) ) {
-//            $paged = $_GET['page'];
-//        }
-//        $query->set( 'paged', $paged );
-//    }
-//}
-//add_action( 'pre_get_posts', 'custom_pagination_base' );
+function custom_pagination_base( $query ) {
+    if ( !is_admin() && ($query->is_main_query() || is_tax('section')) ) {
+        // Check if the `page` GET parameter is set
+        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+        if ( isset( $_GET['page'] ) ) {
+            $paged = $_GET['page'];
+        }
+        $query->set( 'paged', $paged );
+    }
+}
+add_action( 'pre_get_posts', 'custom_pagination_base' );
 
 // Modify pagination base to use a GET parameter
 function resources_archive_custom_query( $query ) {
@@ -110,80 +110,4 @@ function ltm_change_author_base_slug( $args, $taxonomy, $object_type )
     ];
 
     return $args;
-}
-
-add_filter( 'relevanssi_indexing_restriction', 'rlv_exclude_in_house_ad' );
-function rlv_exclude_in_house_ad( $restriction ) {
-    global $wpdb;
-    $restriction['mysql']  .= " AND post.post_type NOT IN ('in-house-ads') ";
-    $restriction['reason'] .= ' In the wrong post type';
-    return $restriction;
-}
-
-add_filter( 'posts_pre_query', 'rlv_modify_found_posts', 999, 2 );
-
-function rlv_modify_found_posts( $posts, $query = false  )
-{
-    global $relevanssi_active;
-
-    if ( ! $query ) {
-        return $posts;
-    }
-
-    $search_ok = true; // We will search!
-    if ( ! $query->is_search() ) {
-        $search_ok = false; // No, we can't, not a search.
-    }
-    if ( ! $query->is_main_query() ) {
-        $search_ok = false; // No, we can't, not the main query.
-    }
-
-    // Uses $wp_query->is_admin instead of is_admin() to help with Ajax queries that
-    // use 'admin_ajax' hook (which sets is_admin() to true whether it's an admin search
-    // or not.
-    if ( $query->is_search() && $query->is_admin ) {
-        $search_ok           = false; // But if this is an admin search, reconsider.
-        $admin_search_option = get_option( 'relevanssi_admin_search' );
-        if ( 'on' === $admin_search_option ) {
-            $search_ok = true; // Yes, we can search!
-        }
-    }
-
-    if ( $query->is_admin && empty( $query->query_vars['s'] ) ) {
-        $search_ok = false; // No search term.
-    }
-
-    if ( $query->is_feed ) {
-        $search_ok = false;
-    }
-
-    if ( $query->get( 'relevanssi' ) ) {
-        $search_ok = true; // Manual override, always search.
-    }
-
-    /**
-     * Filters whether Relevanssi search can be run or not.
-     *
-     * This can be used to for example activate Relevanssi in cases where there is
-     * no search term available.
-     *
-     * @param boolean  True, if Relevanssi can be allowed to run.
-     * @param WP_Query The current query object.
-     */
-    $search_ok = apply_filters( 'relevanssi_search_ok', $search_ok, $query );
-
-    if ( $relevanssi_active ) {
-        $search_ok = false; // Relevanssi is already in action.
-    }
-
-    if ( $search_ok ) {
-        $filteredPosts = [];
-        foreach ($posts as $post) {
-            if($post->post_type === 'in-house-ads') continue;
-            $filteredPosts[] = $post;
-        }
-        return $filteredPosts;
-    }
-
-    return $posts;
 }
