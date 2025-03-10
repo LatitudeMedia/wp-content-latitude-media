e1b553349fa8bc32410803136e0d48dc1415fa49
vlad <vlad.kostetskiy@grandiz.com>
2025-03-10 19:10:57 +0200

    - relevanssi exclude ads

--- wp-content/themes/latitudemedia/inc/override.php
+++ wp-content/themes/latitudemedia/inc/override.php
@@ -112,7 +112,7 @@ function ltm_change_author_base_slug( $args, $taxonomy, $object_type )
     return $args;
 }
 
-add_filter( 'pre_get_posts', 'exclude_ih_house_ads_from_search', 9998, 1 );
+add_filter( 'pre_get_posts', 'exclude_ih_house_ads_from_search', 9, 1 );
 function exclude_ih_house_ads_from_search($query) {
     if ( !is_admin() && $query->is_main_query() && $query->is_search ) {
         $ads = wp_list_pluck( get_posts(['post_type' => 'in-house-ads']), 'ID' );
