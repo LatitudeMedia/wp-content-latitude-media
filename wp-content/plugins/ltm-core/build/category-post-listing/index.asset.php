203a364a9d23215e663409530d2aecec66d5403a
Kim <me@kimdcottrell.com>
2026-08-19 07:23:12 -0400

    mobile styling added

--- wp-content/plugins/ltm-core/build/category-post-listing/index.asset.php
+++ wp-content/plugins/ltm-core/build/category-post-listing/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render'), 'version' => 'eefe40418f696273f361');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render'), 'version' => '8400071b8b9b573bcff9');
